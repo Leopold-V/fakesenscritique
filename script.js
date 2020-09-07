c79f36ce3f30d143a09caf4b45f7e2390cf4615c
Leopold-V <58780217+Leopold-V@users.noreply.github.com>
2020-09-07 16:46:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const wrapper = document.querySelector('.wrapper-result');
 const input_search = document.querySelector('#input_search');
 const form = document.querySelector('form');
 
-//listener searchbar
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     movie = input_search.value;
     getData(movie)
 })
 
-//function execute by listener
 async function getData(movie) {
     wrapper.innerHTML = '<h2 class="mt-5">Loading ...</h2>'
     const rep = await fetch('http://localhost:5000/search/'+movie);
@@ -17,7 +15,6 @@ async function getData(movie) {
     displayData(movies_list);
 }
 
-//function execute after get the data
 function displayData(movies_list) {
     wrapper.innerHTML = ''
     movies_list.map((ele,i) => {
